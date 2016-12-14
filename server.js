2ab890955537da44dcf2067c354870dc48cafacf
Rd-Bhanz <rdudayan2@gmail.com>
2016-12-14 17:52:30 +0530

    Prep repo for heroku

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express'); // access to entire express api
 
 var app = express(); //to create app
+const PORT = process.env.PORT || 3000;
+
+//to redirect https traffic to http because the open weather map api only supports http traffic
+app.use(function(req,res,next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public')); // tell app which folder we want to serve ;'public'(name of folder)
 
-app.listen(3000, function () {    //starting the server at port 3000 and console msg on starting node
-  console.log('Express server is up on 3000');
+app.listen(PORT, function () {    //starting the server at port 3000 and console msg on starting node
+  console.log('Express server is up on '+ PORT);
 });
